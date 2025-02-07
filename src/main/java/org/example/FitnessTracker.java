240139ad98f589ce7dc70d99b52675a874448c79
LiamForsey <liam.forsey@keyin.com>
2025-02-06 21:55:32 -0330

    Created and Finished FitnessTrackerTest.java File, Created more clear and understanding comments, need to edit terminal output, some code in FitnessTracker.java and then do maven adjustments.

--- src/main/java/org/example/FitnessTracker.java
+++ src/main/java/org/example/FitnessTracker.java
@@ -1,10 +1,20 @@
+// This project is for a fitness tracker, however i slightly turned mine into a fat loss journey project...
+// instead of different workouts like weights within, i made it so it shows how many calories you burned,
+// Compared to the calorie goal of your workout. if someones goal is to burn 500 calories for example, if they meet it
+// or if they need to do more to meet it or etc.
+
+
 package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class FitnessTracker {
+
     private final List<Workout> workouts;
+
     private int fitnessGoal; // in calories
 
     public FitnessTracker() {
@@ -12,10 +22,13 @@ public class FitnessTracker {
         fitnessGoal = 0;
     }
 
-    // Log a workout
+
+
+    // This function will Log a workout
     public void logWorkout(String type, int duration) {
         workouts.add(new Workout(type, duration)); // This here adds a new workout to the list
     }
+
 
     // This will get the total calories burned so far
     public int getTotalCaloriesBurned() {
@@ -26,25 +39,35 @@ public class FitnessTracker {
         return total;
     }
 
+
+
     // Set a fitness goal
     public void setFitnessGoal(int goal) {
         this.fitnessGoal = goal;
     }
+
+
 
     // Get the current fitness goal
     public int getFitnessGoal() {
         return fitnessGoal;
     }
 
+
+
     // Check if the goal has been met
     public boolean hasReachedGoal() {
         return getTotalCaloriesBurned() >= fitnessGoal;
     }
 
+
+
     // Get the list of all workouts
     public List<Workout> getWorkouts() {
         return workouts;
     }
+
+
 
     // Print all logged workouts
     public void printWorkouts() {
@@ -54,23 +77,31 @@ public class FitnessTracker {
         }
     }
 
+
+    // code below will configure the type and how long workout is
     public static void main(String[] args) {
         FitnessTracker tracker = new FitnessTracker();
 
-        tracker.logWorkout("Running", 30);
-        tracker.logWorkout("Cycling", 45);
-        tracker.logWorkout("Lifting", 60);
+        tracker.logWorkout("Treadmill", 15);
+        tracker.logWorkout("Bike", 15);
+        tracker.logWorkout("Weightlifting", 45);
 
-        tracker.setFitnessGoal(1000);
+        // This is the goal of calories burned in the session
+        tracker.setFitnessGoal(500);
 
+
+        // printing out results in terminal
         tracker.printWorkouts();
-        System.out.println("Total Calories Burned: " + tracker.getTotalCaloriesBurned());
-        System.out.println("Fitness Goal: " + tracker.getFitnessGoal());
+        System.out.println("Calories Burned: " + tracker.getTotalCaloriesBurned());
+
+        System.out.println("Calorie Goal: " + tracker.getFitnessGoal());
+
 
         if (tracker.hasReachedGoal()) {
-            System.out.println("Nice! You reached your goal!");
+            System.out.println("Nice! You reached your calorie goal!");
         } else {
-            System.out.println("Keep going! You need " + (tracker.getFitnessGoal() - tracker.getTotalCaloriesBurned()) + " more calories.");
+            System.out.println("Keep going! You need " + (tracker.getFitnessGoal() - tracker.getTotalCaloriesBurned()) + " more calories to reach your goal!.");
+
         }
     }
 }
--- src/test/java/FitnessTrackerTest.java
+++ src/test/java/FitnessTrackerTest.java
@@ -1,2 +1,50 @@
+import org.example.FitnessTracker;
+import org.junit.jupiter.api.Test; // JUnit Test annotation
+import static org.junit.jupiter.api.Assertions.*; // Static methods for assertions
+
+
 public class FitnessTrackerTest {
+    @Test
+    void testLogWorkout() {
+        FitnessTracker tracker = new FitnessTracker();
+        tracker.logWorkout("Running", 30);
+        assertEquals(1, tracker.getWorkouts().size(), "There should be 1 workout logged.");
+    }
+
+    @Test
+    void testCalculateTotalCaloriesBurned() {
+        FitnessTracker tracker = new FitnessTracker();
+        tracker.logWorkout("Running", 30);
+        tracker.logWorkout("Cycling",45);
+
+        int totalCalories = tracker.getTotalCaloriesBurned();
+        assertEquals(375, totalCalories);
+    }
+
+    @Test
+    void testSetFitnessGoal() {
+        FitnessTracker tracker = new FitnessTracker();
+        tracker.setFitnessGoal(1000); // Set the goal to burn 1000 calories
+        assertEquals(1000, tracker.getFitnessGoal(), "The fitness goal should be set to 1000.");
+    }
+
+    @Test
+    void testHasReachedGoal_NotYet() {
+        FitnessTracker tracker = new FitnessTracker();
+        tracker.logWorkout("Running", 30); // Burn 150 calories
+        tracker.setFitnessGoal(200); // Goal is set to 200 calories
+
+        assertFalse(tracker.hasReachedGoal(), "The goal should not be reached yet.");
+    }
+
+    @Test
+    void testHasReachedGoal_Achieved() {
+        FitnessTracker tracker = new FitnessTracker();
+        tracker.logWorkout("Running", 30); // Burn 150 calories
+        tracker.logWorkout("Cycling", 45); // Burn 225 calories
+        tracker.setFitnessGoal(375); // Goal is set to 375 calories
+
+        assertTrue(tracker.hasReachedGoal() );
+    }
 }
+
